1be6bf98347b24976aa550000fa0c1be6b6785f5
Divinity <147002921+divine9223@users.noreply.github.com>
2024-10-23 20:27:19 +0200

    Update creation_script.sql
    all tables are now filled

--- lab1/creation_script.sql
+++ lab1/creation_script.sql
@@ -1,21 +1,22 @@
 -- enter code for sql database here
 CREATE TABLE Products (
-    barcode CHAR(15) PRIMARY KEY,
-    product_name CHAR(50) UNIQUE,
-    coffea_species CHAR(20),
-    varietal CHAR(30),
-    origin CHAR(15),
-    roasting_type CHAR(10),
-    is_decaf BOOLEAN,
+    barcode CHAR(15) PRIMARY KEY NOT NULL,
+    product_name CHAR(50) UNIQUE NOT NULL,
+    coffea_species CHAR(20) NOT NULL,
+    varietal CHAR(30) NOT NULL,
+    origin CHAR(15) NOT NULL,
+    roasting_type CHAR(10) NOT NULL,
+    is_decaf BOOLEAN NOT NULL,
     format --how do you point to formats
 );
 
 CREATE TABLE Formats(
-    product_name CHAR(50) UNIQUE,
-    composition CHAR(25),
-    is_prepared BOOLEAN, --capsules or prepared
-    is_volume BOOLEAN, --weight or volume
-    packaging_description NUMBER, --'each format in turn can be packaged differing amounts' == 'packaging description (amount of product)
+    format_id NUMBER PRIMARY KEY NOT NULL,
+    product_name CHAR(50) UNIQUE NOT NULL,
+    composition CHAR(25) NOT NULL,
+    is_prepared BOOLEAN NOT NULL, --capsules or prepared
+    is_volume BOOLEAN NOT NULL, --weight or volume
+    packaging_description NUMBER NOT NULL, --'each format in turn can be packaged differing amounts' == 'packaging description (amount of product)
 );
 
 CREATE TABLE References (
@@ -59,29 +60,8 @@ CREATE TABLE Customer_Orders(
 );
 
 CREATE TABLE Deliveries(
-
-);
-
-CREATE TABLE Addresses(
-
-);
-
-CREATE TABLE Comments (
-    comment_id NUMBER PRIMARY KEY,
-    client_id NUMBER,
-    barcode CHAR(15),
-    post_date DATE,
-    post_time TIMESTAMP,
-    title CHAR(50),
-    text VARCHAR2(2000),
-    score NUMBER(1),
-    likes NUMBER(9),
-    CONSTRAINT fk_comment_client FOREIGN KEY (client_id) REFERENCES Clients(client_id),
-    CONSTRAINT fk_comment_product FOREIGN KEY (barcode) REFERENCES Products(barcode)
-);
-
-CREATE TABLE Credit_Cards(
-
+    address, --needs to point to addresses
+    delivery_date DATE
 );
 
 CREATE TABLE Clients (
@@ -93,7 +73,30 @@ CREATE TABLE Clients (
     address CHAR(100),
     username CHAR(30) UNIQUE,
     password CHAR(30),
-    registration_date DATE
+    registration_date DATE
+);
+
+CREATE TABLE Addresses(
+    stree_num NUMBER(16),
+    thoroughfare_type, --i forgot what this is so idk the var type
+    zip CHAR(10),
+    city CHAR, --idk what the min should be
+    country CHAR(56),
+    gateway NULL, --forgot var type
+    block NULL, --forgot var type and need to change var name
+    stairs_id NULL, --is this a number
+    floor NUMBER(3) NULL,
+    door CHAR(4) NULL,
+    client NULL -- need help pointing
+);
+
+CREATE TABLE Credit_Cards(
+    card_number NUMBER(16),
+    company CHAR(30),
+    cardholder CHAR(30),
+    exp_date CHAR(5), --char max 5 char because its month/year
+    address, --pointer help
+    client --pointer help
 );
 
 CREATE TABLE Orders (
@@ -105,4 +108,19 @@ CREATE TABLE Orders (
     delivery_address CHAR(100),
     payment_type CHAR(20),
     CONSTRAINT fk_client FOREIGN KEY (client_id) REFERENCES Clients(client_id)
+);
+
+CREATE TABLE Comments (
+    comment_id NUMBER PRIMARY KEY,
+    client_id NUMBER,
+    product, --need help pointing
+    format NULL, --need help pointing
+    post_date DATE,
+    post_time TIMESTAMP,
+    title CHAR(50),
+    comment_text VARCHAR2(2000),
+    score NUMBER(1),
+    likes NUMBER(9),
+    CONSTRAINT fk_comment_client FOREIGN KEY (client_id) REFERENCES Clients(client_id),
+    CONSTRAINT fk_comment_product FOREIGN KEY (barcode) REFERENCES Products(barcode)
 );
